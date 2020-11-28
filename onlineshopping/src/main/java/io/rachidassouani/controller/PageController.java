ff6be8161df41ed84ae9ac065dc01b41e036b624
rachidassouani <rachidassouani@gmail.com>
2020-11-28 23:51:03 +0100

    Loaded static resources

---
+++ onlineshopping/src/main/java/io/rachidassouani/controller/PageController.java
@@ -0,0 +1,18 @@
+package io.rachidassouani.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	
+	@RequestMapping(value = {"/", "home", "index"})
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("welcome", "welcome");
+		return modelAndView;
+	}
+
+}
